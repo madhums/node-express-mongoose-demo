e1862302db40d6f2a35da7e42bf7c6b378d41bec
Madhusudhan Srinivasa <madhums8@gmail.com>
2011-08-29 15:02:38 +0000

    remove nodester files

--- nodejs-express-demo/server.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (nodejs-express-demo) is running..');
-}).listen(11629);
