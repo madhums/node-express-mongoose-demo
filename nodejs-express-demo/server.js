597d9c0999256e5ae42591af678264bb54978adc
Madhusudhan Srinivasa <madhums8@gmail.com>
2011-08-29 14:58:55 +0000

    Initial commit via nodester-cli

---
+++ nodejs-express-demo/server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\nApp (nodejs-express-demo) is running..');
+}).listen(11629);
