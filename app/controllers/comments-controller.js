336778e402b25da34ce72633461488bc47859e3a
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-01-22 20:27:15 +0530

    consistent way of doing findX

--- app/controllers/comments-controller.js
+++ app/controllers/comments-controller.js
@@ -5,7 +5,8 @@ var Article = mongoose.model('Article')
 module.exports = function(app){
   app.param('articleId', function(req, res, next, id){
     Article
-      .findOne({ _id : id }, function(err,article) {
+      .findOne({ _id : id })
+      .run(function(err,article) {
         if (err) return next(err);
         if (!article) return next(new Error('Failed to load article ' + id));
         req.article = article;
