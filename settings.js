fe37d42ba004115826ca7e92d0194198ea75ec7d
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-01-22 19:59:26 +0530

    add back gzippo

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    //gzippo = require('gzippo'),
-    mongoStore = require('connect-mongodb'),
-    mongooseAuth = require('mongoose-auth'),
-    url = require('url');
+var express = require('express')
+  , gzippo = require('gzippo')
+  , mongoStore = require('connect-mongodb')
+  , mongooseAuth = require('mongoose-auth')
+  , url = require('url')
 
 exports.boot = function(app){
-  bootApplication(app);
-};
+  bootApplication(app)
+}
 
 // App settings and middleware
 
@@ -19,9 +19,9 @@ function bootApplication(app) {
   app.configure(function(){
 
     // set views path, template engine and default layout
-    app.set('views', __dirname + '/app/views');
-    app.set('view engine', 'jade');
-    app.set('view options', { layout: 'layouts/default' });
+    app.set('views', __dirname + '/app/views')
+    app.set('view engine', 'jade')
+    app.set('view options', { layout: 'layouts/default' })
 
     // contentFor & content view helper - to include blocks of content only on required pages
     app.use(function(req, res, next){
@@ -30,57 +30,57 @@ function bootApplication(app) {
 
       // assign content str for section
       res.local('contentFor', function(section, str){
-        res.local(section, str);
-      });
+        res.local(section, str)
+      })
 
       // check if the section is defined and return accordingly
       res.local('content', function(section){
         if (typeof res.local(section) != 'undefined')
-          return res.local(section);
+          return res.local(section)
         else
-          return '';
-      });
+          return ''
+      })
 
-      next();
-    });
+      next()
+    })
 
     // bodyParser should be above methodOverride
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    app.use(express.bodyParser())
+    app.use(express.methodOverride())
 
     // cookieParser should be above session
-    app.use(express.cookieParser());
+    app.use(express.cookieParser())
     app.use(express.session({
       secret: 'noobjs',
       store: new mongoStore({
         url: config.db.uri,
         collection : 'sessions'
       })
-    }));
+    }))
 
-    app.use(express.logger(':method :url :status'));
-    app.use(express.favicon());
+    app.use(express.logger(':method :url :status'))
+    app.use(express.favicon())
 
     // routes should be at the last
-    // app.use(app.router);
-    app.use(mongooseAuth.middleware());
-  });
+    // app.use(app.router)
+    app.use(mongooseAuth.middleware())
+  })
 
   // Setup ejs views as default, with .html as the extension
-  //app.set('views', __dirname + '/views');
-  //app.register('.html', require('ejs'));
-  //app.set('view engine', 'html');
+  //app.set('views', __dirname + '/views')
+  //app.register('.html', require('ejs'))
+  //app.set('view engine', 'html')
 
   // Some dynamic view helpers
   app.dynamicHelpers({
 
     request: function(req){
-      return req;
+      return req
     },
 
     hasMessages: function(req){
-      if (!req.session) return false;
-      return Object.keys(req.session.flash || {}).length;
+      if (!req.session) return false
+      return Object.keys(req.session.flash || {}).length
     },
 
     // flash messages
@@ -88,11 +88,11 @@ function bootApplication(app) {
 
     // dateformat helper. Thanks to gh-/loopj/commonjs-date-formatting
     dateformat: function(req, res) {
-      return require('./lib/dateformat').strftime;
+      return require('./lib/dateformat').strftime
     },
 
     base: function(){
-      return '/' == app.route ? '' : app.route; // return the app's mount-point so that urls can adjust.
+      return '/' == app.route ? '' : app.route // return the app's mount-point so that urls can adjust.
     },
 
     appName : function(req, res) {
@@ -103,32 +103,32 @@ function bootApplication(app) {
       return 'nodejs express mongoose demo'
     }
 
-  });
+  })
 
   // Don't use express errorHandler as we are using custom error handlers
-  // app.use(express.errorHandler({ dumpExceptions: false, showStack: false }));
+  // app.use(express.errorHandler({ dumpExceptions: false, showStack: false }))
 
   // show error on screen. False for all envs except development
   // settmgs for custom error handlers
-  app.set('showStackError', false);
+  app.set('showStackError', false)
 
   // configure environments
 
   app.configure('development', function(){
-    app.set('showStackError', true);
-    app.use(express.static(__dirname + '/public'));
-  });
+    app.set('showStackError', true)
+    app.use(express.static(__dirname + '/public'))
+  })
 
   // gzip only in staging and production envs
 
   app.configure('staging', function(){
-    //app.use(gzippo.staticGzip(__dirname + '/public'));
-    app.enable('view cache');
-  });
+    app.use(gzippo.staticGzip(__dirname + '/public'))
+    app.enable('view cache')
+  })
 
   app.configure('production', function(){
-    //app.use(gzippo.staticGzip(__dirname + '/public'));
+    app.use(gzippo.staticGzip(__dirname + '/public'))
     // view cache is enabled by default in production mode
-  });
+  })
 
 }
