da209d384da55e10c1c5da1d83bec9bc06755b0d
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-01-22 20:28:23 +0530

    add user to comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,10 +1,10 @@
 // comment schema
 
 var Comments = new Schema({
-  body        : {type : String, default : ''},
-  article     : {type : Schema.ObjectId, ref : 'Article'},
-  created_at  : {type : Date, default : Date.now},
-  updated_at  : {type : Date, default : Date.now}
-});
+    body        : {type : String, default : ''}
+  , article     : {type : Schema.ObjectId, ref : 'Article'}
+  , user        : {type : Schema.ObjectId, ref : 'User'}
+  , created_at  : {type : Date, default : Date.now}
+})
 
-mongoose.model('Comment', Comments);
+mongoose.model('Comment', Comments)
