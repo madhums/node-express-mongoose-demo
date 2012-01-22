48f32dd64f3b2ae90c741a30324687814bfa292c
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-01-22 20:28:03 +0530

    add user to articles

--- app/models/atricle.js
+++ app/models/atricle.js
@@ -1,19 +1,19 @@
 // Article schema
 
 var ArticleSchema = new Schema({
-  title       : {type : String, default : '', trim : true},
-  body        : {type : String, default : '', trim : true},
-  created_at  : {type : Date, default : Date.now},
-  updated_at  : {type : Date, default : Date.now}
-});
+    title       : {type : String, default : '', trim : true}
+  , body        : {type : String, default : '', trim : true}
+  , user        : {type : Schema.ObjectId, ref : 'User'}
+  , created_at  : {type : Date, default : Date.now}
+})
 
 ArticleSchema.path('title').validate(function (title) {
-  return title.length > 0;
-}, 'Article title cannot be blank');
+  return title.length > 0
+}, 'Article title cannot be blank')
 
 ArticleSchema.path('body').validate(function (body) {
-  return body.length > 0;
-}, 'Article body cannot be blank');
+  return body.length > 0
+}, 'Article body cannot be blank')
 
 
-mongoose.model('Article', ArticleSchema);
+mongoose.model('Article', ArticleSchema)
