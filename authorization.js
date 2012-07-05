ce01f4d793dc09bbe1c92a2cb05a38459d257015
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-07-06 05:07:36 +0530

    fix authorizations

--- authorization.js
+++ authorization.js
@@ -33,10 +33,10 @@ exports.user = {
 
 exports.article = {
     hasAuthorization : function (req, res, next) {
-      if (req.article.user.id != req.session.auth.userId) {
+      if (req.article.user._id.toString() != req.user._id.toString()) {
         req.flash('notice', 'You are not authorized');
         res.redirect('/article/'+req.article.id);
       }
       next()
     }
-}  
+}
