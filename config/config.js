eed033646ef259998a2b8282777ab70b21f15fa9
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-12 17:02:34 +0530

    use simple js instead of yaml

---
+++ config/config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    development: {
+      db: 'mongodb://localhost/noobjs_dev'
+    }
+  , test: {
+      db: 'mongodb://localhost/noobjs_test'
+    }
+  , production: {
+      db: 'mongodb://neocom:neocom@alex.mongohq.com:10014/app6633199'
+    }
+}
