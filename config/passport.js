9a34273e0d6e4c77260c90a9815637b1299df26a
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-15 22:28:17 +0530

    configure passport for github twitter and facebook

---
+++ config/passport.js
@@ -0,0 +1,113 @@
+
+var mongoose = require('mongoose')
+  , LocalStrategy = require('passport-local').Strategy
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GitHubStrategy = require('passport-github').Strategy
+  , User = mongoose.model('User')
+
+
+exports.boot = function (passport, config) {
+
+  require('./initializer')
+
+  // serialize sessions
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function(id, done) {
+    User.findOne({ _id: id }, function (err, user) {
+      done(err, user)
+    })
+  })
+
+  // use local strategy
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {
+          return done(null, false, { message: 'Unknown user' })
+        }
+        if (!user.authenticate(password)) {
+          return done(null, false, { message: 'Invalid password' })
+        }
+        return done(null, user)
+      })
+    }
+  ))
+
+  // use twitter strategy
+  passport.use(new TwitterStrategy({
+      consumerKey: config.twitter.consumerKey,
+      consumerSecret: config.twitter.consumerSecret,
+      callbackURL: config.twitter.callbackURL
+    },
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {
+          return done(null, false, { message: 'Unknown user' })
+        }
+        if (!user.authenticate(password)) {
+          return done(null, false, { message: 'Invalid password' })
+        }
+        return done(null, user)
+      })
+    }
+  ))
+
+  // use facebook strategy
+  passport.use(new FacebookStrategy({
+        clientID: config.facebook.appId
+      , clientSecret: config.facebook.appSecret
+      , callbackURL: config.facebook.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {
+          user = new User({
+              name: profile.displayName
+            , email: profile.emails[0].value
+            , username: profile.username
+            , provider: 'facebook'
+            , facebook: profile._json
+          })
+          user.save(function (err, user) {
+            if (err) console.log(err)
+            return done(err, user)
+          })
+        }
+        else {
+          return done(err, user)
+        }
+      })
+    }
+  ))
+
+  // use github strategy
+  passport.use(new GitHubStrategy({
+      clientID: config.github.appId,
+      clientSecret: config.github.secret,
+      callbackURL: config.github.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      var User = mongoose.model('User')
+      User.findOne({ 'github.id': profile.id }, function (err, user) {
+        if (!user) {
+          user = new User({
+              name: profile.displayName
+            , email: profile.emails[0].value
+            , username: profile.username
+            , provider: 'github'
+            , github: profile._json
+          })
+          user.save()
+        }
+        return done(err, user)
+      })
+    }
+  ))
+}
