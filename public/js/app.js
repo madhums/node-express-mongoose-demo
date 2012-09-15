96c094b939241c0c3b31fd2cbcbceb28c6c15714
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-16 04:04:49 +0530

    add bootbox confirmation

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  // confirmations
+  $('.confirm').submit(function (e) {
+    e.preventDefault();
+    var self = this;
+    var msg = 'Are you sure?';
+    bootbox.confirm(msg, 'cancel', 'Yes! I am sure', function (action) {
+      if (action) {
+        $(self).unbind('submit');
+        $(self).trigger('submit');
+      }
+    });
+  });
+
+});
