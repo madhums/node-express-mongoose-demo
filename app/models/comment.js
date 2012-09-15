e94b8a6e4ad3082eafc9888009040a40b8ad7e2c
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-15 22:31:47 +0530

    modify comment schema

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,10 +1,12 @@
 // comment schema
 
-var Comments = new Schema({
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+var CommentSchema = new Schema({
     body        : {type : String, default : ''}
-  , article     : {type : Schema.ObjectId, ref : 'Article'}
   , user        : {type : Schema.ObjectId, ref : 'User'}
   , created_at  : {type : Date, default : Date.now}
 })
 
-mongoose.model('Comment', Comments)
+mongoose.model('Comment', CommentSchema)
