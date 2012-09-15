1d40766eb15fbbfd3656437b68f43b1292a3e1cf
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-15 22:41:32 +0530

    modify articles model

--- app/models/atricle.js
+++ app/models/atricle.js
@@ -1,10 +1,14 @@
 // Article schema
 
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
 var ArticleSchema = new Schema({
-    title       : {type : String, default : '', trim : true}
-  , body        : {type : String, default : '', trim : true}
-  , user        : {type : Schema.ObjectId, ref : 'User'}
-  , created_at  : {type : Date, default : Date.now}
+    title: {type : String, default : '', trim : true}
+  , body: {type : String, default : '', trim : true}
+  , user: {type : Schema.ObjectId, ref : 'User'}
+  , comments: [{type : Schema.ObjectId, ref : 'Comment'}]
+  , createdAt  : {type : Date, default : Date.now}
 })
 
 ArticleSchema.path('title').validate(function (title) {
@@ -14,6 +18,5 @@ ArticleSchema.path('title').validate(function (title) {
 ArticleSchema.path('body').validate(function (body) {
   return body.length > 0
 }, 'Article body cannot be blank')
-
 
 mongoose.model('Article', ArticleSchema)
