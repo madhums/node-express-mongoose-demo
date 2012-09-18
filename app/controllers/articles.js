ea4263b9dff489f072e3fb343fd84bbcd9fb5281
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-09-18 16:54:17 +0530

    add pagination

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -84,16 +84,25 @@ exports.destroy = function(req, res){
 
 // Listing of Articles
 exports.index = function(req, res){
+  var perPage = 5
+    , page = req.param('page') > 0 ? req.param('page') : 0
+
   Article
     .find({})
     .populate('user')
     .populate('comments')
-    .sort({'created_at': -1}) // sort by date
+    .sort({'createdAt': -1}) // sort by date
+    .limit(perPage)
+    .skip(perPage * page)
     .exec(function(err, articles) {
-      if (err) throw err
-      res.render('articles/index', {
-        title: 'List of Articles',
-        articles: articles
+      if (err) return res.render('500')
+      Article.count().exec(function (err, count) {
+        res.render('articles/index', {
+            title: 'List of Articles'
+          , articles: articles
+          , page: page
+          , pages: count / perPage
+        })
       })
     })
 }
--- settings.js
+++ settings.js
@@ -38,6 +38,26 @@ function bootApplication(app, config, passport) {
       res.locals.stripScript = function (str) {
         return str.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
       }
+      res.locals.createPagination = function (pages, page) {
+        var url = require('url')
+          , qs = require('querystring')
+          , params = qs.parse(url.parse(req.url).query)
+          , str = ''
+
+        params.page = 0
+        var clas = page == 0 ? "active" : "no"
+        str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">First</a></li>'
+        for (var p = 1; p < pages; p++) {
+          params.page = p
+          clas = page == p ? "active" : "no"
+          str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">'+ p +'</a></li>'
+        }
+        params.page = --p
+        clas = page == params.page ? "active" : "no"
+        str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">Last</a></li>'
+
+        return str
+      }
 
       next()
     })
