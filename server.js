4fec4e7bd2c3c805900b7cb3145451d05335cda6
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-12-25 21:52:25 +0100

    cleanup

--- server.js
+++ server.js
@@ -5,30 +5,26 @@ var express = require('express')
   , fs = require('fs')
   , passport = require('passport')
 
-require('express-namespace')
-
 // Load configurations
 var env = process.env.NODE_ENV || 'development'
   , config = require('./config/config')[env]
-  , auth = require('./authorization')
+  , auth = require('./config/authorization')
 
 // Bootstrap db connection
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
 mongoose.connect(config.db)
 
 // Bootstrap models
 var models_path = __dirname + '/app/models'
-  , model_files = fs.readdirSync(models_path)
-model_files.forEach(function (file) {
+fs.readdirSync(models_path).forEach(function (file) {
   require(models_path+'/'+file)
 })
 
 // bootstrap passport config
 require('./config/passport').boot(passport, config)
 
-var app = express()                                       // express app
-require('./settings').boot(app, config, passport)         // Bootstrap application settings
+var app = express()
+// express settings
+require('./config/express').boot(app, config, passport)
 
 // Bootstrap routes
 require('./config/routes')(app, passport, auth)
