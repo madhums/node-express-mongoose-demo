39e85d517eea83ce3b365b34a4ad87949e4e65f0
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-04-20 14:47:28 +0530

    make use of add comment method

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -15,12 +15,7 @@ exports.create = function (req, res) {
 
   if (!req.body.body) return res.redirect('/articles/'+ article.id)
 
-  article.comments.push({
-    body: req.body.body,
-    user: user._id
-  })
-
-  article.save(function (err) {
+  article.addComment(user, req.body, function (err) {
     if (err) return res.render('500')
     res.redirect('/articles/'+ article.id)
   })
