5e646970fca2db38b4b7cc14158fa2917236611d
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-04-20 14:46:55 +0530

    update config to add notifier and cleanup

--- config/config.example.js
+++ config/config.example.js
@@ -1,36 +1,46 @@
 
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
+  , notifier = {
+      APN: false,
+      email: false, // true
+      actions: ['comment'],
+      tplPath: templatePath,
+      postmarkKey: 'POSTMARK_KEY',
+      parseAppId: 'PARSE_APP_ID',
+      parseApiKey: 'PARSE_MASTER_KEY'
+    }
+
 module.exports = {
-    development: {
-      root: require('path').normalize(__dirname + '/..'),
-      app: {
-        name: 'Nodejs Express Mongoose Demo'
-      },
-      db: 'mongodb://localhost/noobjs_dev',
-      facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/facebook/callback"
-      },
-      twitter: {
-          clientID: "CONSUMER_KEY"
-        , clientSecret: "CONSUMER_SECRET"
-        , callbackURL: "http://localhost:3000/auth/twitter/callback"
-      },
-      github: {
-          clientID: 'APP_ID'
-        , clientSecret: 'APP_SECRET'
-        , callbackURL: 'http://localhost:3000/auth/github/callback'
-      },
-      google: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/google/callback"
-      }
-    }
-  , test: {
-
-    }
-  , production: {
-
-    }
+  development: {
+    db: 'mongodb://localhost/noobjs_dev',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/google/callback"
+    },
+  },
+  test: {},
+  production: {}
 }
