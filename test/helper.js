0fcedca7e22c0d705c65fe166bb4e0f8f0ee29d2
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-04-21 14:50:40 +0530

    add tests helper

---
+++ test/helper.js
@@ -0,0 +1,13 @@
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , env = process.env.NODE_ENV || 'development'
+  , config = require('../config/config')[env]
+  , modelPath = config.root + '/app/models'
+  , async = require('async')
+
+require('fs').readdirSync(modelPath).forEach(function (file) {
+  require(modelPath + '/' + file)
+})
