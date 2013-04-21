c14d0a89b7bb016c75ba61e41edac1110ae7bf5a
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-04-21 14:56:57 +0530

    add test env configs

--- config/config.example.js
+++ config/config.example.js
@@ -41,6 +41,33 @@ module.exports = {
       callbackURL: "http://localhost:3000/auth/google/callback"
     },
   },
-  test: {},
+  test: {
+    db: 'mongodb://localhost/noobjs_test',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/google/callback"
+    }
+  },
   production: {}
 }
