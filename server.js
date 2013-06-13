796adcb95cd72bd3407a735d226dad7cefff4c21
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-06-13 18:13:35 +0530

    require only js files. fixes #49

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(config.db)
 // Bootstrap models
 var models_path = __dirname + '/app/models'
 fs.readdirSync(models_path).forEach(function (file) {
-  require(models_path+'/'+file)
+  if (~file.indexOf('.js')) require(models_path + '/' + file)
 })
 
 // bootstrap passport config
