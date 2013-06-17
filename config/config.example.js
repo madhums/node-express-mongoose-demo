33968f7bb6337dc13d484095ac90b4d278f97df6
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-06-17 16:23:51 +0530

    update change in notifier api

--- config/config.example.js
+++ config/config.example.js
@@ -3,11 +3,12 @@ var path = require('path')
   , rootPath = path.normalize(__dirname + '/..')
   , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
   , notifier = {
+      service: 'postmark',
       APN: false,
       email: false, // true
       actions: ['comment'],
       tplPath: templatePath,
-      postmarkKey: 'POSTMARK_KEY',
+      key: 'POSTMARK_KEY',
       parseAppId: 'PARSE_APP_ID',
       parseApiKey: 'PARSE_MASTER_KEY'
     }
