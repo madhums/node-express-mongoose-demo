ec0735378744b98298f8dc5bb770edad4be762e7
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-06-18 18:07:30 +0530

    reset perPage to 30

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -117,7 +117,7 @@ exports.destroy = function(req, res){
 
 exports.index = function(req, res){
   var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
-  var perPage = 4
+  var perPage = 30
   var options = {
     perPage: perPage,
     page: page
