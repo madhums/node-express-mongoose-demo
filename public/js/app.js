d39a381b58d0f328e73fb0d6e20757b0def7425e
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-12-31 22:48:25 +0100

    update client side stuff, add confirmation for deletion of content

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
 
-  // confirmations
-  $('.confirm').submit(function (e) {
-    e.preventDefault();
-    var self = this;
-    var msg = 'Are you sure?';
-    bootbox.confirm(msg, 'cancel', 'Yes! I am sure', function (action) {
-      if (action) {
-        $(self).unbind('submit');
-        $(self).trigger('submit');
-      }
-    });
-  });
-
   $('#tags').tagsInput({
     'height':'60px',
     'width':'280px'
