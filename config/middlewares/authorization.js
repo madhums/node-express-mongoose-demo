4991c965ec102ed6b6ad12790e8e3978804d4332
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-12-31 22:30:39 +0100

    remember only GET operations

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -4,11 +4,9 @@
  */
 
 exports.requiresLogin = function (req, res, next) {
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl
-    return res.redirect('/login')
-  }
-  next()
+  if (req.isAuthenticated()) return next()
+  if (req.method == 'GET') req.session.returnTo = req.originalUrl
+  res.redirect('/login')
 }
 
 /*
