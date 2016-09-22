ba77a16e358ae1dfce33d49607323c42d9d24e47
Madhusudhan Srinivasa <madhums8@gmail.com>
2016-09-22 11:53:07 +0200

    add ability to filter list with a single item

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -31,11 +31,14 @@ exports.load = async(function* (req, res, next, id) {
 
 exports.index = async(function* (req, res) {
   const page = (req.query.page > 0 ? req.query.page : 1) - 1;
+  const _id = req.query.item;
   const limit = 30;
   const options = {
     limit: limit,
     page: page
   };
+
+  if (_id) options.criteria = { _id };
 
   const articles = yield Article.list(options);
   const count = yield Article.count();
