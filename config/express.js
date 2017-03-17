f7eba07681ac834fce19e4c42b097f4ca572289f
Madhusudhan Srinivasa <madhums8@gmail.com>
2017-03-18 00:39:24 +0100

    add cors options to support fetch

--- config/express.js
+++ config/express.js
@@ -36,7 +36,10 @@ module.exports = function (app, passport) {
     threshold: 512
   }));
 
-  app.use(cors());
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }));
 
   // Static files middleware
   app.use(express.static(config.root + '/public'));
