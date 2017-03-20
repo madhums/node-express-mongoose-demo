16beb70f4e0bda37868d7a082cb9137b36facb0b
Madhusudhan Srinivasa <madhums8@gmail.com>
2017-03-20 22:58:49 +0100

    assign to empty object to not alter the original

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ const defaults = {
  */
 
 module.exports = {
-  development: Object.assign(development, defaults),
-  test: Object.assign(test, defaults),
-  production: Object.assign(production, defaults)
+  development: Object.assign({}, development, defaults),
+  test: Object.assign({}, test, defaults),
+  production: Object.assign({}, production, defaults)
 }[process.env.NODE_ENV || 'development'];
