9fffe894732dcdb15f812f0fdad77575c90bdd0a
Madhusudhan Srinivasa <madhums8@gmail.com>
2019-03-13 18:03:27 +0100

    fix tests

--- config/middlewares/require-https.js
+++ config/middlewares/require-https.js
@@ -8,7 +8,8 @@ module.exports = function requireHTTPS(req, res, next) {
   if (
     !req.secure &&
     req.get('x-forwarded-proto') !== 'https' &&
-    process.env.NODE_ENV !== 'development'
+    process.env.NODE_ENV !== 'development' &&
+    process.env.NODE_ENV !== 'test'
   ) {
     return res.redirect('https://' + req.get('host') + req.url);
   }
