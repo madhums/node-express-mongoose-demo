12ed2a0cc00467b7b2681e23f433101f19a33e54
Madhusudhan Srinivasa <madhums8@gmail.com>
2019-03-13 17:05:05 +0100

    use https

--- config/env/production.js
+++ config/env/production.js
@@ -10,28 +10,30 @@ module.exports = {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL:
-      'http://nodejs-express-demo.herokuapp.com/auth/facebook/callback'
+      'https://nodejs-express-demo.herokuapp.com/auth/facebook/callback'
   },
   twitter: {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
     callbackURL:
-      'http://nodejs-express-demo.herokuapp.com/auth/twitter/callback/'
+      'https://nodejs-express-demo.herokuapp.com/auth/twitter/callback/'
   },
   github: {
     clientID: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/github/callback'
+    callbackURL:
+      'https://nodejs-express-demo.herokuapp.com/auth/github/callback'
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENTID,
     clientSecret: process.env.LINKEDIN_SECRET,
     callbackURL:
-      'http://nodejs-express-demo.herokuapp.com/auth/linkedin/callback'
+      'https://nodejs-express-demo.herokuapp.com/auth/linkedin/callback'
   },
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: 'http://nodejs-express-demo.herokuapp.com/auth/google/callback'
+    callbackURL:
+      'https://nodejs-express-demo.herokuapp.com/auth/google/callback'
   }
 };
