7b86f526c597d52dda39d45f0405466c8b0bd2df
Madhusudhan Srinivasa <me@madhums.me>
2019-03-28 01:19:55 +0100

    closes #218

--- config/env/test.js
+++ config/env/test.js
@@ -4,26 +4,28 @@
  * Expose
  */
 
+const port = process.env.PORT || 3000;
+
 module.exports = {
   db: process.env.MONGODB_URL || 'mongodb://localhost/noobjs_test',
   twitter: {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
-    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    callbackURL: `http://localhost:${port}/auth/twitter/callback`
   },
   github: {
     clientID: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
+    callbackURL: `http://localhost:${port}/auth/github/callback`
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENTID,
     clientSecret: process.env.LINKEDIN_SECRET,
-    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    callbackURL: `http://localhost:${port}/auth/linkedin/callback`
   },
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: `http://localhost:${port}/auth/google/callback`
   }
 };
