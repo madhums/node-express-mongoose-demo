b5b786e1f3cf8d79d419771c87b919bd461decee
madhums <me@madhums.me>
2023-08-31 11:51:29 +0200

    update dockerfile for fly.io

--- Dockerfile
+++ Dockerfile
@@ -36,4 +36,4 @@ COPY --from=build /app /app
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
-CMD [ "npm", "run", "start" ]
+CMD [ "npm", "run", "prod" ]
--- config/env/production.js
+++ config/env/production.js
@@ -9,25 +9,21 @@ module.exports = {
   twitter: {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
-    callbackURL:
-      'https://nodejs-express-demo.fly.dev/auth/twitter/callback/'
+    callbackURL: 'https://nodejs-express-demo.fly.dev/auth/twitter/callback/',
   },
   github: {
     clientID: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_SECRET,
-    callbackURL:
-      'https://nodejs-express-demo.fly.dev/auth/github/callback'
+    callbackURL: 'https://nodejs-express-demo.fly.dev/auth/github/callback',
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENTID,
     clientSecret: process.env.LINKEDIN_SECRET,
-    callbackURL:
-      'https://nodejs-express-demo.fly.dev/auth/linkedin/callback'
+    callbackURL: 'https://nodejs-express-demo.fly.dev/auth/linkedin/callback',
   },
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL:
-      'https://nodejs-express-demo.fly.dev/auth/google/callback'
-  }
+    callbackURL: 'https://nodejs-express-demo.fly.dev/auth/google/callback',
+  },
 };
