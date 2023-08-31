867e359d6465f029974494a3abaab58377f053dd
madhums <me@madhums.me>
2023-08-31 11:46:53 +0200

    migrate from heroku to fly.io

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+# syntax = docker/dockerfile:1
+
+# Adjust NODE_VERSION as desired
+ARG NODE_VERSION=18.16.1
+FROM node:${NODE_VERSION}-slim as base
+
+LABEL fly_launch_runtime="Node.js"
+
+# Node.js app lives here
+WORKDIR /app
+
+# Set production environment
+ENV NODE_ENV="production"
+
+
+# Throw-away build stage to reduce size of final image
+FROM base as build
+
+# Install packages needed to build node modules
+RUN apt-get update -qq && \
+    apt-get install -y build-essential pkg-config python-is-python3
+
+# Install node modules
+COPY --link package-lock.json package.json ./
+RUN npm ci
+
+# Copy application code
+COPY --link . .
+
+
+# Final stage for app image
+FROM base
+
+# Copy built application
+COPY --from=build /app /app
+
+# Start the server by default, this can be overwritten at runtime
+EXPOSE 3000
+CMD [ "npm", "run", "start" ]
--- config/env/production.js
+++ config/env/production.js
@@ -10,24 +10,24 @@ module.exports = {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
     callbackURL:
-      'https://nodejs-express-demo.herokuapp.com/auth/twitter/callback/'
+      'https://nodejs-express-demo.fly.dev/auth/twitter/callback/'
   },
   github: {
     clientID: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_SECRET,
     callbackURL:
-      'https://nodejs-express-demo.herokuapp.com/auth/github/callback'
+      'https://nodejs-express-demo.fly.dev/auth/github/callback'
   },
   linkedin: {
     clientID: process.env.LINKEDIN_CLIENTID,
     clientSecret: process.env.LINKEDIN_SECRET,
     callbackURL:
-      'https://nodejs-express-demo.herokuapp.com/auth/linkedin/callback'
+      'https://nodejs-express-demo.fly.dev/auth/linkedin/callback'
   },
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL:
-      'https://nodejs-express-demo.herokuapp.com/auth/google/callback'
+      'https://nodejs-express-demo.fly.dev/auth/google/callback'
   }
 };
